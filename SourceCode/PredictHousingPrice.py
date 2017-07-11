dab5591f7bda412030bf53bcf9cb0c51e3885272
Nguyen Hong <hongnguyen060895@gmail.com>
2017-07-12 01:05:02 +0700

    chạy thực nghiệm công trình liên quan

--- SourceCode/PredictHousingPrice.py
+++ SourceCode/PredictHousingPrice.py
@@ -1,5 +1,9 @@
 print(1)
 import pandas as pd
+import numpy as np
+from IPython.display import HTML, display
+# import statsmodels.api as sm
+from statsmodels.formula.api import ols
 # read in from csv using pd.read_csv
 # be sure to use the file path where you saved the data
 
@@ -12,19 +16,24 @@ df = shiller.merge(housing_price_index, on='date')\
                     .merge(unemployment, on='date')\
                     .merge(federal_funds_rate, on='date')\
                     .merge(gross_domestic_product, on='date')
-train = df[1:50]
-print(train)
 
-from IPython.display import HTML, display
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-housing_model = ols("""housing_price_index ~ total_unemployed 
+
+
+for count in range(1,20,4): 
+    test = df[count:count+4] 
+    if(count > 4 and count < 21 ):
+      train = df[1:count].append(df[count+4:22])
+    else:
+        train = df[5:22]
+    housing_model = ols("""housing_price_index ~ total_unemployed 
                                             + long_interest_rate 
                                             + federal_funds_rate
                                             + consumer_price_index 
                                             + gross_domestic_product""", data=train).fit()
-
-
-predict_model_summary = housing_model.summary()
-predictions = housing_model.predict(train)
-print(predictions)
+    predict_model_summary = housing_model.summary()
+    # print(predict_model_summary)
+    predictions = housing_model.predict(test)
+    # print(predictions)
+    price_index = test.housing_price_index
+    lech = abs(price_index - predictions)/price_index
+    print(100 - np.mean(lech)*100)
